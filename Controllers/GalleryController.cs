266c8bc173b9c44fc1018f1a071bf0fb99a6fd54
Ben Barr <benbarr@windowslive.com>
2021-09-26 22:46:15 +1300

    Made Razor Pages, updated links

---
+++ Controllers/GalleryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1054686___Individual_Assignment.Controllers
+{
+    [Route("Gallery")]
+    public class GalleryController: Controller
+    {
+
+           [Route("index")]
+            public IActionResult Index()
+            {
+                return View();
+            }
+        
+    }
+}
